67829212dde07fd98313acc4a63ea71ea0cab8f8
Zeeshan Abbas <67509023+zeeshansGitHub@users.noreply.github.com>
2021-04-27 10:43:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+app.listen(3005, function () {
+console.log("listening on 3005");
+});
+app.get("/", (req, res) => {
+res.send("Users Shown");
+});
+app.get("/delete", (req, res) => {
+res.send("Delete User");
+});
+app.get("/update", (req, res) => {
+res.send("Update User");
+});
+app.get("/insert", (req, res) => {
+res.send("Insert User");
+});
